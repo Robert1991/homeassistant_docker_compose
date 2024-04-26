e4e8ec1baee7e62971af3635cec2723827f6bdd2
Robert1991 <rwaddell91@googlemail.com>
2024-04-26 17:35:08 +0200

    added nginx proxy to docker env to have ssl handling outside home assistant

--- generate_certificates.sh
+++ generate_certificates.sh
@@ -1,10 +1,14 @@
-sudo docker run --rm -it --rm -p 80:80 --name certbot \
-        -v "/home/robert/docker/homeassistant/certificates:/etc/letsencrypt" \
-        -v "/home/robert/docker/homeassistant/volumes/letsencrypt:/var/lib/letsencrypt" \
-        -e URL=robertnator.duckdns.org \
-        -e EMAIL=rwaddell91@googlemail.com \
-        -e VALIDATION=http \
-        -e DUCKDNSTOKEN=06f592bf-6673-46a8-8b3a-c0a8951a5d67 \
-	certbot/certbot certonly --standalone -d robertnator.duckdns.org --preferred-challenges http-01 
-#        linuxserver/swag renew –agree-tos
+source ./.env
 
+docker run --rm -it --rm -p 80:80 --name certbot \
+        -v "$(pwd)/certificates:/etc/letsencrypt" \
+        -v "$(pwd)/certificates/letsencrypt_volume:/var/lib/letsencrypt" \
+        -e URL=$DUCKDNS_DOMAIN \
+        -e EMAIL=$LETSENCRYPT_EMAIL \
+        -e VALIDATION=http \
+        -e DUCKDNSTOKEN=$DUCKDNS_TOKEN \
+        certbot/certbot certonly --standalone -d $DUCKDNS_DOMAIN --preferred-challenges http-01 --force-renewal
+
+docker stop home-assistant_nginx
+docker rm home-assistant_nginx
+docker-compose up -d
